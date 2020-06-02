bc992412cea95d6e93f7cdc2e1b51f4b2c1c1746
snowjet <>
2020-06-02 18:57:24 +1000

    reverting to basic just show response header

--- main.py
+++ main.py
@@ -7,28 +7,12 @@ from flask import session
 from flask import url_for
 import json
 
-import jwt
-import os
 
 app = Flask(__name__)
 
-def decode_token(auth_token):
-
-  SECRET_KEY = os.getenv("SECRET_KEY", "random")
-  payload = jwt.decode(auth_token, SECRET_KEY)
-  
-  return payload
-
 @app.route('/')
 def index():
-  print(request.headers)
 
   headers = dict(request.headers)
-
-  if 'X-Auth-Token' in headers:
-    token = decode_token(auth_token=headers['X-Auth-Token'])
-    print(token)
-
-  print(json.dumps(headers))
 
   return jsonify(headers)
