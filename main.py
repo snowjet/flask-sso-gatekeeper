1acd3d25aa19678ef3c2edba726424193e2fb618
Rarm Jet <rarm.jet@gmail.com>
2020-06-02 15:58:32 +1000

    try and decode auth token

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ from flask import session
 from flask import url_for
 import json
 
+import jwt
+
 app = Flask(__name__)
+
+def decode_token(auth_token):
+
+  SECRET_KEY = os.getenv("SECRET_KEY", "random key")
+  payload = jwt.decode(auth_token, SECRET_KEY)
+  
+  return payload
 
 @app.route('/')
 def index():
   print(request.headers)
 
   headers = dict(request.headers)
+
+  if 'X-Auth-Token' in headers:
+    token = decode_token(auth_token=headers['X-Auth-Token'])
+    print(token)
+
+  print(json.dumps(headers))
 
   return jsonify(headers)
