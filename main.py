4447dbd6a265b86d8736ea1f9d32b011490e0863
Rarm Jet <rarm.jet@gmail.com>
2020-06-02 14:43:59 +1000

    fixed all the missing flask imports

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import request
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
