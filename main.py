701292be0630383525722534c468cfb850af5b6e
rarm <rarm.jet@gmail.com>
2020-11-26 17:03:48 +1100

    fixed duplicate

--- main.py
+++ main.py
@@ -47,17 +47,6 @@ def is_admin(f):
     return decorated
 
 
-def decode_jwt(auth_token, aud):
-
-    try:
-        PUBLIC_KEY = os.getenv("PUBLIC_KEY", "RSA Public Key")
-        aud = aud.split(",")
-        payload = jwt.decode(auth_token, PUBLIC_KEY, audience=aud)
-
-        return "verified"
-    except:
-        return "verification failed with - %s" % sys.exc_info()[0]
-
 def decode_jwt(auth_token):
 
     try:
